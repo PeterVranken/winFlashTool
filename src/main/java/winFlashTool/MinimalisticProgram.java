1f08f035be7feec8ade726ba76dbdde58c214008
vranken <vranken@fev.io>
2025-10-23 11:28:42 +0200

    Time measurements added to find out, why the permanent polling doesn't generate high CPU load.
    Finding: Without CAN Rx, the code takes about 3.38 us per polling cycle and spends 3.23 us inside of PCANBasic.Read. If we don't have an effect of the CPU load measurement in Task Manager then it seems that the call of Read has an implicit none-busy wait for the result of about 3 us. This short release of the CPU may suffice to avoid blocking the entire CPU.

--- src/main/java/winFlashTool/MinimalisticProgram.java
+++ src/main/java/winFlashTool/MinimalisticProgram.java
@@ -2,9 +2,13 @@ package winFlashTool;
 
 import java.lang.StringBuffer;
 import peak.can.basic.*;
+import org.apache.logging.log4j.*;
 
 public class MinimalisticProgram
 {
+    /** The global logger object for all progress and error reporting. */
+    private static final Logger _logger = LogManager.getLogger(MinimalisticProgram.class);
+
     public static void main(String[] args)
     {
         PCANBasic can = null;
@@ -12,10 +16,10 @@ public class MinimalisticProgram
         can = new PCANBasic();
         if(!can.initializeAPI())
         {
-            System.out.println("Unable to initialize the API");
-            System.exit(0);
+            _logger.error("Unable to initialize the API");
+            return;
         }
-        
+
         /* Arguments 3..5 are not used for the Plug&Play device PEAK-USB and PEAK-USB-FD.
            We set them to "don't care". */
         status = can.Initialize( TPCANHandle.PCAN_USBBUS1
@@ -26,18 +30,62 @@ public class MinimalisticProgram
                                );
         final TPCANMsg msg = new TPCANMsg();
         byte byte0 = 0;
-        while(status == TPCANStatus.PCAN_ERROR_OK
-              ||  status == TPCANStatus.PCAN_ERROR_QRCVEMPTY
+        long tiReadMax = 0
+           , tiReadMin = Long.MAX_VALUE
+           , tiReadAvg = 0
+           , noRead = 0;
+        long tiReadEmptyQMax = 0
+           , tiReadEmptyQMin = Long.MAX_VALUE
+           , tiReadEmptyQAvg = 0
+           , noReadEmptyQ = 0;
+        long tiExe = System.nanoTime()
+           , noCycles = 0;
+        TimeoutTimer timeoutNoRx = new TimeoutTimer(/*timeoutMillis*/ 1000*10);
+        while((status == TPCANStatus.PCAN_ERROR_OK
+               ||  status == TPCANStatus.PCAN_ERROR_QRCVEMPTY
+              )
+              && !timeoutNoRx.hasTimedOut()
              )
         {
+            ++ noCycles;
+
             int noMsgsInQ = 0;
             //while(can.Read(TPCANHandle.PCAN_USBBUS1, msg, null) == TPCANStatus.PCAN_ERROR_OK)
             while(true)
             {
+                final long tiStart = System.nanoTime();
                 status = can.Read(TPCANHandle.PCAN_USBBUS1, msg, null);
+                final long tiRead = System.nanoTime() - tiStart;
+
                 if(status != TPCANStatus.PCAN_ERROR_OK)
+                {
+                    /* noCycles > 10: Avoid unrealistic maximum due to setup and
+                       initialization effects. */
+                    if(status == TPCANStatus.PCAN_ERROR_QRCVEMPTY &&  noCycles > 10)
+                    {
+                        ++ noReadEmptyQ;
+                        tiReadEmptyQAvg += tiRead;
+                        if(tiRead > tiReadEmptyQMax)
+                            tiReadEmptyQMax = tiRead;
+                        if(tiRead < tiReadEmptyQMin)
+                            tiReadEmptyQMin = tiRead;
+                    }
+
                     break;
-                 
+                }
+
+                timeoutNoRx.restart();
+
+                if(noCycles > 10)
+                {
+                    ++ noRead;
+                    tiReadAvg += tiRead;
+                    if(tiRead > tiReadMax)
+                        tiReadMax = tiRead;
+                    if(tiRead < tiReadMin)
+                        tiReadMin = tiRead;
+                }
+
                 ++ noMsgsInQ;
 
                 /* To see the effect of our SW, we modify the message. */
@@ -58,17 +106,44 @@ public class MinimalisticProgram
 
                 status = can.Write(TPCANHandle.PCAN_USBBUS1, msg);
                 if(status != TPCANStatus.PCAN_ERROR_OK)
-                    System.out.println("Unable to write the CAN message");
+                    _logger.error("Unable to write the CAN message");
             }
             //if(noMsgsInQ > 1)
             //    System.out.printf("noMsgsInQ: %d\n", noMsgsInQ);
-            //System.out.printf("Loop left because of status %d\n", status.getValue());
+            //_logger.info("Loop left because of status {}", status.getValue());
         }
-        
+
+        tiExe = System.nanoTime() - tiExe;
+        _logger.info( "Executed {} polling cycles, average execution time of cycle is {} ns"
+                    , noCycles
+                    , tiExe / noCycles
+                    );
+
         /* Translate last error into human understandable text. */
         StringBuffer errMsg = new StringBuffer();
         status = can.GetErrorText(status, /*Language*/ (short)0, errMsg);
         assert status == TPCANStatus.PCAN_ERROR_OK: "Error in decoding error message";
-        System.out.println("Last error: " + errMsg.toString());
+        _logger.error("Last error: {}", errMsg.toString());
+
+        assert noReadEmptyQ > 0: "This simple programm assumes a minimum runtime";
+        _logger.info( "Timing of can.Read() (all measures in ns):\n"
+                      + "CAN message received:\n"
+                      + "  Max: {}\n"
+                      + "  Min: {}\n"
+                      + "  Avg: {}\n"
+                      + "Empty queue:\n"
+                      + "  Max: {}\n"
+                      + "  Min: {}\n"
+                      + "  Avg: {}\n"
+                      + "Both modes in average:\n"
+                      + "  Avg: {}\n"
+                    , tiReadMax
+                    , noRead > 0? tiReadMin: 0
+                    , noRead > 0? tiReadAvg / noRead: 0
+                    , tiReadEmptyQMax
+                    , tiReadEmptyQMin
+                    , tiReadEmptyQAvg / noReadEmptyQ
+                    , (tiReadEmptyQAvg + tiReadAvg) / (noRead + noReadEmptyQ)
+                    );
     }
 }
